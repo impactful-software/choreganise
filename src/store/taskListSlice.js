2fbe9979679c22e206ad1873e251b2df90e575f2
David Bingham <git@davidjbingham.co.uk>
2022-03-17 16:25:37 +0000

    Don't purge cached task list on reset
    It's a better user experience to see the old tasks while the new tasks
    are loading with a spinner overlaid and interaction disabled.
    Resetting the fetch status triggers a new fetch when required.

--- src/store/taskListSlice.js
+++ src/store/taskListSlice.js
@@ -126,7 +126,6 @@ export const taskListSlice = createSlice({
     },
     resetTasks: (state, action) => {
       state.status.fetchTasks = initialState.status.fetchTasks
-      state.tasks = initialState.tasks
     }
   },
   extraReducers(builder) {
